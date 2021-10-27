9c32695fe25142dc89fdd12d59f72e4694baecb5
Lukasz Solarz <lukaszsolarz@icloud.com>
2021-10-27 18:29:07 +0200

    add print objects

--- index.php
+++ index.php
@@ -9,22 +9,40 @@
 <body id="body">
 <div id="container">
 </div>
+<form class="form" method="post">
+       Imię: <input type="text"  name="name">
+    <button type="submit">Wyślij</button>
+</form>
+
 <script>
     console.log("hello its me")
     fetch("peopleData.json")
     .then(response => response.json())
     .then(data =>{
-    for(var i=0; i<data.length; i++)
-    {
-        if(data[i].first_name=='Timmy')
+        console.log(data)
+        for(var i=0; i<data.length; i++)
         {
-            console.log(data[i])
-            break;
+            if(data[i].first_name=='Timmy')
+            {
+                console.log(data[i])
+                document.getElementById("id_first_name").innerHTML =data[i].first_name;
+                document.getElementById("id_last_name").innerHTML =data[i].last_name;
+                document.getElementById("id_gender").innerHTML =data[i].gender;
+                document.getElementById("id_age").innerHTML =data[i].age;
+                document.getElementById("id_height").innerHTML =data[i].height;
+                break;
+            }
         }
-    }
     })
 
+
 </script>
+<div id="id_first_name"></div>
+<div id="id_last_name"></div>
+<div id="id_gender"></div>
+<div id="id_age"></div>
+<div id="id_height"></div>
+
 </div>
 </body>
 </html>
