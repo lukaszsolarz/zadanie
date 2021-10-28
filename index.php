649f5a6464d5ac78c45f759c8a8bc5cd17dfcfb6
Lukasz Solarz <lukaszsolarz@icloud.com>
2021-10-28 21:20:51 +0200

    final version

--- index.php
+++ index.php
@@ -4,52 +4,92 @@
     <meta charset="utf-8"/>
     <meta http-equiv="X-UA-Compatible">
     <title> zadanie</title>
+    <link rel="stylesheet" href="style.css"/>
+
 </head>
-<link rel="stylesheet" href="styleCss.css"/>
 <body id="body">
-<div id="container">
 </div>
 <form class="form" method="post">
-       Imię: <input type="text"  name="fname">
-    <button type="submit">Wyślij</button>
+    Imię dziecka: <input type="text" name="fname">
+    <button type="submit">Sprawdź</button>
 </form>
 <?
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$name = $_POST['fname'];
-if (empty($name)) {
-echo "Sorry name is empty";
-}}
+    $name = $_POST['fname'];
+    if (empty($name)) {
+        echo '<div id="id_warning">Proszę wprowadzić imię dziecka</div>';
+    }
+}
 ?>
-
 <script>
 
-    console.log("hello its me")
+    function show_ok_icon() {
+        var ok_icon = document.createElement("IMG");
+        ok_icon.setAttribute("src", "ok.png");
+        ok_icon.setAttribute("width", "40");
+        ok_icon.setAttribute("height", "40");
+        document.body.appendChild(ok_icon)
+    }
+
+    function show_Nok_icon() {
+        var nok_icon = document.createElement("IMG");
+        nok_icon.setAttribute("src", "nok.png");
+        nok_icon.setAttribute("width", "40");
+        nok_icon.setAttribute("height", "40");
+        document.body.appendChild(nok_icon)
+    }
+
     fetch("peopleData.json")
-    .then(response => response.json())
-    .then(data =>{
-        console.log(data)
-        for(var i=0; i<data.length; i++)
-        {
-            if(data[i].first_name =="<?php echo $name; ?>")
-            {
-                console.log(data[i])
-                document.getElementById("id_first_name").innerText =data[i].first_name;
-                document.getElementById("id_last_name").innerHTML =data[i].last_name;
-                document.getElementById("id_gender").innerHTML =data[i].gender;
-                document.getElementById("id_age").innerHTML =data[i].age;
-                document.getElementById("id_height").innerHTML =data[i].height;
-                break;
+        .then(response => response.json())
+        .then(data => {
+            for (var i=0; i<data.length; i++) {
+
+                if (data[i].first_name == "<?php echo $name; ?>") {
+                    var name_children = data[i].first_name
+                    var age_children = data[i].age
+                    var height_children = data[i].height
+
+                    if (data[i].gender == "M") {
+                        fetch("normForMen.json")
+                            .then(response => response.json())
+                            .then(data => {
+                                    var max_height = (data[age_children].max)
+                                    var min_height = (data[age_children].min)
+                                    if (height_children >= min_height && height_children <= max_height) {
+                                        document.getElementById("id_result").innerHTML = (name_children + " height is correct")
+                                        show_ok_icon();
+                                    } else {
+                                        document.getElementById("id_result").innerHTML = (name_children + " height is incorrect. Height is " + height_children + "cm and should be between " + min_height + " and " + max_height + "cm")
+                                        show_Nok_icon();
+                                    }
+                                }
+                            )
+                    } else {
+                        fetch("normForWomen.json")
+                            .then(response => response.json())
+                            .then(data => {
+                                    var max_height = (data[age_children].max)
+                                    var min_height = (data[age_children].min)
+                                    if (height_children >= min_height && height_children <= max_height) {
+                                        document.getElementById("id_result").innerHTML = (name_children + " height is correct")
+                                        show_ok_icon();
+                                    } else {
+                                        document.getElementById("id_result").innerHTML = (name_children + " height is incorrect. Height is " + height_children + "cm and should be between " + min_height + "and " + max_height + "cm")
+                                        document.getElementById("id_result")
+                                        show_Nok_icon();
+                                    }
+
+                                }
+                            )
+                    }
+                }
             }
-        }
-    })
+        })
 
 </script>
-<br><br>
-name: <div id="id_first_name"></div> <br>
-last name: <div id="id_last_name"></div><br>
-gender: <div id="id_gender"></div><br>
-age: <div id="id_age"></div><br>
-height: <div id="id_height"></div><br>
+<br>
+WYNIK:
+<div id="id_result"></div>
 
 </div>
 </body>
